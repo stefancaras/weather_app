94d3c212252107184222a4c2859d13f252ae5e98
Stefan Caras <stefan.caras@gmail.com>
2022-10-02 21:49:04 +0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q="
-const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q="
+import "./leaflet/leaflet.js";
+
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
 let weatherData = {};
 let forecastData = {};
-const input = document.querySelector(".input")
+let lat, long;
+const input = document.querySelector(".input");
 
 const getWeatherData = async () => {
   const result = await fetch(`${weatherURL}${input.value}`);
 	weatherData = await result.json();
   showWeather();
+
+  // Show map
+  lat = weatherData.coord.lat;
+  long = weatherData.coord.lon;
+  showMap();
 }
 
 const getForecastData = async () => {
@@ -16,30 +24,29 @@ const getForecastData = async () => {
   showForecast();
 }
 
-const getWeather = () => {
+document.querySelector(".btn").addEventListener("click", () => {
   getWeatherData();
   getForecastData();
-}
-
-document.querySelector(".btn").addEventListener("click", getWeather)
+})
 
 const showWeather = () => {
   document.querySelector(".weather").innerHTML = `
-    <div class="">
-      <h2>${weatherData.name}, ${weatherData.sys.country}</h2>
-      <img src="http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png" />
-      <p>Description: ${weatherData.weather[0].description}</p>
-      <p>Humidity: ${weatherData.main.humidity} %</p>
-      <p>Pressure: ${weatherData.main.pressure} hPa</p>
-      <p>Current Temp: ${weatherData.main.temp} &#8451</p>
-      <p>Min Temp: ${weatherData.main.temp_min} &#8451</p>
-      <p>Max Temp: ${weatherData.main.temp_max} &#8451</p>
-    </div>
-    <div class="">
-      <iframe width="100%" height="100%" frameborder="0" style="border:0"
-        src="https://maps.google.com/maps?hl=en&amp;q=${weatherData.name},${weatherData.sys.country}+()&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
-      </iframe>
+    <div class="flexWrapCenter">
+      <div class="flexWrapCenter column">
+        <h2>${weatherData.name}, ${weatherData.sys.country}</h2>
+        <div class="flexWrapCenter">
+          <img src="http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png" />
+          <span class="largeFont">${weatherData.main.temp} &#8451</span>
+        </div>
+        <p>Feels like: ${weatherData.main.feels_like} &#8451</p>
+        <p>Description: ${weatherData.weather[0].description}</p>
+        <p>Humidity: ${weatherData.main.humidity} %</p>
+        <p>Pressure: ${weatherData.main.pressure} hPa</p>
+        <p>Wind speed: ${weatherData.wind.speed} m/s</p>
+      </div>
+      <div id="map"></div>
     </div>`
+    console.log(weatherData)
 }
 
 const showForecast = () => {
@@ -54,4 +61,11 @@ const showForecast = () => {
         <p>Description: ${forecastData.list[0].weather[0].description}</p>
       </div>`
   console.log(forecastData)
+}
+
+const showMap = () => {
+  var map = L.map('map').setView([lat, long], 7);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, 
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 }
